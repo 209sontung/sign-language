cd5c6e9410002bf64e1d9a7aaf6ed7de960a5f50
st <209tungns@gmail.com>
2023-05-10 10:48:23 +0700

    placeholder commit

--- main.py
+++ main.py
@@ -1,40 +1,126 @@
+from backbone import TFLiteModel, get_model
+from utils import read_json_file, load_relevant_data_subset
+
 import tensorflow as tf
 import numpy as np
 import pandas as pd
 import json
+import cv2
 import os
+import mediapipe as mp
+
 from multiprocessing import cpu_count
 
-def read_json_file(file_path):
-    """Read a JSON file and parse it into a Python object.
 
-    Args:
-        file_path (str): The path to the JSON file to read.
+# train_df = pd.read_csv('/Users/sontung/Documents /MyDocuments/NEU/Thesis/sign_language/resources/train.csv')
+# print("\n\n... LOAD SIGN TO PREDICTION INDEX MAP FROM JSON FILE ...\n")
+# s2p_map = {k.lower():v for k,v in read_json_file(os.path.join("/Users/sontung/Documents /MyDocuments/NEU/Thesis/sign_language/resources/sign_to_prediction_index_map.json")).items()}
+# p2s_map = {v:k for k,v in read_json_file(os.path.join("/Users/sontung/Documents /MyDocuments/NEU/Thesis/sign_language/resources/sign_to_prediction_index_map.json")).items()}
+# encoder = lambda x: s2p_map.get(x.lower())
+# decoder = lambda x: p2s_map.get(x)
+# print(s2p_map)
+# train_df['label'] = train_df.sign.map(encoder)
 
-    Returns:
-        dict: A dictionary object representing the JSON data.
-        
-    Raises:
-        FileNotFoundError: If the specified file path does not exist.
-        ValueError: If the specified file path does not contain valid JSON data.
-    """
-    try:
-        # Open the file and load the JSON data into a Python object
-        with open(file_path, 'r') as file:
-            json_data = json.load(file)
-        return json_data
-    except FileNotFoundError:
-        # Raise an error if the file path does not exist
-        raise FileNotFoundError(f"File not found: {file_path}")
-    except ValueError:
-        # Raise an error if the file does not contain valid JSON data
-        raise ValueError(f"Invalid JSON data in file: {file_path}")
+
+mp_holistic = mp.solutions.holistic # holistic model
+mp_drawing = mp.solutions.drawing_utils # drawing utilities
+
+def mediapipe_detection(image, model):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # color conversion
+    image.flags.writeable = False # img no longer writeable
+    pred = model.process(image) # make landmark prediction
+    image.flags.writeable = True  # img now writeable
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR) # color reconversion
+    return image, pred
+
+def draw(image, results):
+    mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION,
+                              mp_drawing.DrawingSpec(color=(0,0,255), thickness=3, circle_radius=3),
+                              mp_drawing.DrawingSpec(color=(0,0,0), thickness=1, circle_radius=0))
+    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
+                              mp_drawing.DrawingSpec(color=(0,150,0), thickness=3, circle_radius=3),
+                              mp_drawing.DrawingSpec(color=(0,0,0), thickness=2, circle_radius=2))
+    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                              mp_drawing.DrawingSpec(color=(200,56,12), thickness=3, circle_radius=3),
+                              mp_drawing.DrawingSpec(color=(0,0,0), thickness=2, circle_radius=2))
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                              mp_drawing.DrawingSpec(color=(250,56,12), thickness=3, circle_radius=3),
+                              mp_drawing.DrawingSpec(color=(0,0,0), thickness=2, circle_radius=2))
     
-train_df = pd.read_csv('/Users/sontung/Documents /MyDocuments/NEU/Thesis/sign_language/resources/train.csv')
-print("\n\n... LOAD SIGN TO PREDICTION INDEX MAP FROM JSON FILE ...\n")
-s2p_map = {k.lower():v for k,v in read_json_file(os.path.join("/Users/sontung/Documents /MyDocuments/NEU/Thesis/sign_language/resources/sign_to_prediction_index_map.json")).items()}
-p2s_map = {v:k for k,v in read_json_file(os.path.join("/Users/sontung/Documents /MyDocuments/NEU/Thesis/sign_language/resources/sign_to_prediction_index_map.json")).items()}
+def extract_coordinates(results):
+    face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]) if results.face_landmarks.landmark else np.zeros((468, 3))
+    pose = np.array([[res.x, res.y, res.z] for res in results.pose_landmarks.landmark]) if results.pose_landmarks.landmark else np.zeros((33, 3))
+    lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]) if results.left_hand_landmarks.landmark else np.zeros((21, 3))
+    rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]) if results.right_hand_landmarks.landmark else np.zeros((21, 3))
+    return np.concatenate([face, lh, pose, rh])
+    
+def load_json_file(json_path):
+    with open(json_path, 'r') as f:
+        sign_map = json.load(f)
+    return sign_map
+
+class CFG:
+    data_dir = "/Users/sontung/Documents /MyDocuments/NEU/Thesis/sign_language/resources/"
+    sequence_length = 12
+    rows_per_frame = 543
+
+ROWS_PER_FRAME = 543
+def load_relevant_data_subset(pq_path):
+    data_columns = ['x', 'y', 'z']
+    data = pd.read_parquet(pq_path, columns=data_columns)
+    n_frames = int(len(data) / ROWS_PER_FRAME)
+    data = data.values.reshape(n_frames, ROWS_PER_FRAME, len(data_columns))
+    return data.astype(np.float32)
+    
+sign_map = load_json_file(CFG.data_dir + 'sign_to_prediction_index_map.json')
+train_data = pd.read_csv(CFG.data_dir + 'train.csv')
+
+s2p_map = {k.lower():v for k,v in load_json_file(CFG.data_dir + "sign_to_prediction_index_map.json").items()}
+p2s_map = {v:k for k,v in load_json_file(CFG.data_dir + "sign_to_prediction_index_map.json").items()}
 encoder = lambda x: s2p_map.get(x.lower())
 decoder = lambda x: p2s_map.get(x)
-print(s2p_map)
-train_df['label'] = train_df.sign.map(encoder)
+
+models_path = [
+                '/Users/sontung/Documents /MyDocuments/NEU/Thesis/sign_language/islr-fp16-192-8-seed42-fold0-best.h5',
+]
+models = [get_model() for _ in models_path]
+for model,path in zip(models,models_path):
+    model.load_weights(path)
+
+
+
+
+def real_time_asl():
+    tflite_keras_model = TFLiteModel(islr_models=models)
+
+    sequence_data = []
+    cap = cv2.VideoCapture(0)
+    with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+        while cap.isOpened():
+            ret, frame = cap.read()
+            image, results = mediapipe_detection(frame, holistic)
+            draw(image, results)
+
+            try:
+                landmarks = extract_coordinates(results)
+            except:
+                landmarks = np.zeros((468 + 21 + 33 + 21, 3))
+            sequence_data.append(landmarks)
+            
+            sign = "None"
+            
+            if len(sequence_data) % 15 == 0:
+                prediction = tflite_keras_model(np.array(sequence_data, dtype = np.float32))["outputs"]
+                sign = np.argmax(prediction.numpy(), axis=-1)
+                
+            cv2.putText(image, f"Prediction:    {decoder(sign)}", (3, 30),
+                            cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+                
+            cv2.imshow('Webcam Feed',image)
+            if cv2.waitKey(10) & 0xFF == ord("q"):
+                break
+
+        cap.release()
+        cv2.destroyAllWindows()
+
+real_time_asl()
